172b4bdc07b2aed5c99165607ee2d8d9d1da1d6f
Jack Rimmer <jack.rimmer@matillion.com>
2023-08-25 18:21:47 +0100

    Remove some comments

--- FAQ-API/src/main/java/com/faq/Security/SecurityConfig.java
+++ FAQ-API/src/main/java/com/faq/Security/SecurityConfig.java
@@ -30,9 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // configure AuthenticationManager so that it knows from where to load
-        // user for matching credentials
-        // Use BCryptPasswordEncoder
         auth.userDetailsService(accountService).passwordEncoder(passwordEncoder());
     }
 
