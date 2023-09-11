1cb5567854514cc29f3c788331afc30721a525d6
Jack Rimmer <rimbojnr@gmail.com>
2023-09-11 18:21:18 +0100

    FE Add file structure

--- FAQ-FE/faq-app/src/Components/App/App.tsx
+++ FAQ-FE/faq-app/src/Components/App/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
-import Signup from './Signup';
+import Home from '../Home/Home';
+import Login from '../Login/Login';
+import Signup from '../Signup/Signup';
 
 function App() {
   return (
